db21fa40f4a18fe63303a87eb8127ea247320f32
knwng <ken.qrose@gmail.com>
2017-12-15 00:26:22 +0800

    add backend code

--- Backend.py
+++ Backend.py
@@ -1,33 +1,69 @@
 import numpy as np
 import socket
+from config import *
 
 
 class Comm2Server(object):
-    def __init__(self, dst_ip, dst_port):
+    def __init__(self, dst_ip, dst_port, account):
         # self.dst_ip = dst_ip
         # self.dst_port = dst_port
         self.address = (dst_ip, dst_port)
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    def comm(self, data):
+        self.account = account
         self.sock.connect(self.address)
         print('Socket name: ', self.sock.getsockname())
-        self.sock.sendall(data)
-        reply = rcvall(self.sock, 1024)
+
+    def login(self):
+        self.sock.sendall(self.account+'_net2017')
+        reply = self.sock.recv(BUFFERSIZE)
+        # print('reply: ', reply)
+        # self.sock.close()
+        return reply == 'lol'
+
+    def logout(self):
+        # self.sock.connect(self.address)
+        self.sock.sendall('logout'+self.account)
+        reply = self.sock.recv(BUFFERSIZE)
+        # print('reply: ', reply)
         self.sock.close()
+        return reply == 'loo'
+
+    def query(self, faccount):
+        # self.sock.connect(self.address)
+        self.sock.sendall('q'+faccount)
+        reply = self.sock.recv(BUFFERSIZE)
+        print('reply: ', reply)
+        # self.sock.close()
+        if reply == 'n':
+            return None
         return reply
 
 
 class CommP2P(object):
-    def __init__(self):
+    def __init__(self, dst_ip, dst_port):
+        self.address = (dst_ip, dst_port)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect(self.address)
+
+    def sendText(self, data):
+        self.sock.sendall(data)
+        return
+
+    def rcvText(self):
+        return self.sock.recv(BUFFERSIZE)
+
+    def sendFile(self, filename):
         pass
 
 
+'''
 def rcvall(sock, buffer_length):
     data = b''
     while len(data) < buffer_length:
         remain = sock.recv(buffer_length - len(data))
-        if not remain:
-            raise EOFError('buffer size is %d but only received %d '.format(buffer_length, len(data)))
+        if not len(remain):
+            break
+            # raise EOFError('buffer size is %d but only received %d '.format(buffer_length, len(data)))
         data += remain
     return data
+'''
--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from Backend import *
+from config import *
+import time
 
 
 def main():
-    client = Comm2Server('166.111.140.14', 8000)
-    # reply = client.comm(b'2014010622_net2017')
-    reply = client.comm(b'2015010173_net2017')
-    print(reply)
+    client = Comm2Server('166.111.140.14', 8000, '2014010622')
+    # friend = Comm2Server('166.111.140.14', 8000, '2014011653')
+    print ('Login: ', client.login())
+    # print ('Friend Login: ', friend.logout())
+    print ('query: ', client.query('2014011653'))
+    print ('query: ', client.query('2014010622'))
+    print ('Logout: ', client.logout())
+    # print ('Friend Logout: ', friend.logout())
 
 
 if __name__ == '__main__':
